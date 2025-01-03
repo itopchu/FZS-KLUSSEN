8660f216979a84937ba4067b96a42ab5cd82d134
Ibrahim Topchu <mr.ibrahim.topchu@outlook.com>
2025-01-03 06:44:49 +0100

    app-bar slidable solved

--- services/front/vite-react-ts/src/Sections/Header/index.tsx
+++ services/front/vite-react-ts/src/Sections/Header/index.tsx
@@ -77,7 +77,7 @@ function Header() {
           boxShadow: `0 0.2em 0.1 ${theme.palette.text.secondary}`,
           zIndex: theme.zIndex.drawer + 1,
           maxWidth: '100%',
-          overflowX: 'hidden',
+          overflow: 'hidden',
         }}
       >
         <Toolbar sx={{ height: '95%' }}>
